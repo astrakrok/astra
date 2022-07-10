2a4fecee0c596b24d9b7091bee11c4642dc8bb0f
AndriiBosyk <andriybosik2002@ukr.net>
2022-07-10 18:19:07 +0300

    Add CORS configuration

--- astra-api/src/main/java/com/example/astraapi/AstraApiApplication.java
+++ astra-api/src/main/java/com/example/astraapi/AstraApiApplication.java
@@ -1,9 +1,12 @@
 package com.example.astraapi;
 
+import com.example.astraapi.config.CorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CorsProperties.class)
 public class AstraApiApplication {
 
   public static void main(String[] args) {
