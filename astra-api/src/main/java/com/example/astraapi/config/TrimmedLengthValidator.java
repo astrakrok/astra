f973df912b5beef51a60aba5840aaca285a818e5
YuraKovalenko <urakovalenko001@gmail.com>
2022-07-11 19:51:50 +0300

    minor fix

--- astra-api/src/main/java/com/example/astraapi/config/TrimmedLengthValidator.java
+++ astra-api/src/main/java/com/example/astraapi/config/TrimmedLengthValidator.java
@@ -18,10 +18,7 @@ public class TrimmedLengthValidator implements ConstraintValidator<TrimmedLength
   @Override
   public boolean isValid(String value, ConstraintValidatorContext context) {
     int trimmedValueLength = getTrimmedValueLength(value);
-    if ( trimmedValueLength>= this.max) {
-      return false;
-    }
-    return trimmedValueLength >= this.min;
+    return trimmedValueLength <= this.max && trimmedValueLength >= this.min;
   }
 
   private int getTrimmedValueLength(String value) {
--- astra-api/src/main/java/com/example/astraapi/mapper/SpecializationMapper.java
+++ astra-api/src/main/java/com/example/astraapi/mapper/SpecializationMapper.java
@@ -2,10 +2,13 @@ package com.example.astraapi.mapper;
 
 import com.example.astraapi.dto.SpecializationDto;
 import com.example.astraapi.entity.SpecializationEntity;
+import com.example.astraapi.mapper.qualifier.SpecializationQualifier;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = SpecializationQualifier.class)
 public interface SpecializationMapper {
+  @Mapping(target = "title", qualifiedByName = SpecializationQualifier.TRIM)
   SpecializationEntity toEntity(SpecializationDto specializationDto);
 
   SpecializationDto toDto(SpecializationEntity specializationEntity);
