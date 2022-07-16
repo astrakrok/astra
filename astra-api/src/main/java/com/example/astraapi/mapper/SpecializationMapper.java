6c0f71d6036de9b6af15822c6944242b8a5b7a1c
YuraKovalenko <urakovalenko001@gmail.com>
2022-07-16 12:46:08 +0300

    Add validation for subjects

--- astra-api/src/main/java/com/example/astraapi/mapper/SpecializationMapper.java
+++ astra-api/src/main/java/com/example/astraapi/mapper/SpecializationMapper.java
@@ -2,13 +2,13 @@ package com.example.astraapi.mapper;
 
 import com.example.astraapi.dto.SpecializationDto;
 import com.example.astraapi.entity.SpecializationEntity;
-import com.example.astraapi.mapper.qualifier.SpecializationQualifier;
+import com.example.astraapi.mapper.qualifier.TitleQualifier;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring", uses = SpecializationQualifier.class)
+@Mapper(componentModel = "spring", uses = TitleQualifier.class)
 public interface SpecializationMapper {
-  @Mapping(target = "title", qualifiedByName = SpecializationQualifier.TRIM)
+  @Mapping(target = "title", qualifiedByName = TitleQualifier.TRIM)
   SpecializationEntity toEntity(SpecializationDto specializationDto);
 
   SpecializationDto toDto(SpecializationEntity specializationEntity);
