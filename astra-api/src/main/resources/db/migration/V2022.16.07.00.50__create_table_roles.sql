9e93c66c95d921f7d11d58c7e0ceb7812a71afbe
AndriiBosyk <andriybosik2002@ukr.net>
2022-07-19 17:14:51 +0300

    Remove redundant migrations

--- astra-api/src/main/resources/db/migration/V2022.16.07.00.50__create_table_roles.sql
+++
@@ -1,6 +0,0 @@
-create type role_types as enum('user', 'admin', 'superadmin');
-
-create table roles(
-	id bigserial primary key,
-	role role_types	
-);
--- astra-api/src/main/resources/db/migration/V2022.16.07.00.50__create_table_users_roles.sql
+++
@@ -1,7 +0,0 @@
-create table public.users_roles
-(
-	user_id int unique not null,
-	foreign key (user_id) references public.users (id),
-	role_id int unique not null,
-	foreign key (role_id) references public.roles (id)
-);
