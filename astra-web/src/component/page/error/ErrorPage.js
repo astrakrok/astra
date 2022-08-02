aa170caee1da65b1410d22cea75e177d328acd2c
AndriiBosyk <andriybosik2002@ukr.net>
2022-08-02 23:58:09 +0300

    Add pages for displaying errors

--- astra-web/src/component/page/error/ErrorPage.js
+++ astra-web/src/component/page/error/ErrorPage.js
@@ -1,18 +1,22 @@
 import {page} from "../../../constant/page";
 import Button from "../../Button/Button";
-import withTitle from "../../hoc/withTitle/withTitle";
+import Logo from "../../Logo/Logo";
 import Spacer from "../../Spacer/Spacer";
-import "./NotFoundPage.css";
+import "./ErrorPage.css";
 
-const NotFoundPage = () => {
+const ErrorPage = () => {
     return (
-        <div className="NotFoundPage full-width">
+        <div className="ErrorPage full-width">
             <div className="window-height s-hflex-center">
                 <div className="full-height s-vflex-center center">
-                    <div className="code">404</div>
-                    <div className="message">Сторінку не знайдено</div>
+                    <div className="s-hflex-center">
+                        <Logo />
+                    </div>
+                    <div className="message">
+                        Щось пішло не так. Вибачте за незручності та спробуйте ще раз
+                    </div>
                     <div className="s-hflex-center navigation">
-                        <Button to={page.home} type="secondary" isFilled={true}>
+                        <Button to={page.home} isFilled={true} type="secondary">
                             <div className="s-hflex">
                                 <i className="material-icons">arrow_back</i>
                                 <Spacer width={8} />
@@ -26,4 +30,4 @@ const NotFoundPage = () => {
     );
 }
 
-export default withTitle(NotFoundPage, "Сторінку не знайдено");
+export default ErrorPage;
