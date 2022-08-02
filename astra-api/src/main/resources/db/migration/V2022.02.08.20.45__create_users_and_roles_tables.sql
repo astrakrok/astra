d5c2fe5cc5b169279551385ec24874b12f8370ed
AndriiBosyk <andriybosik2002@ukr.net>
2022-08-02 20:52:56 +0300

    Formatting fix

--- astra-api/src/main/resources/db/migration/V2022.02.08.20.45__create_users_and_roles_tables.sql
+++ astra-api/src/main/resources/db/migration/V2022.02.08.20.45__create_users_and_roles_tables.sql
@@ -1,24 +1,28 @@
 create table public.users
 (
-    id bigserial primary key ,
-    name varchar(50) not null,
-    surname varchar(50) not null,
-    email varchar(50) not null,
-    course varchar(10) not null,
-    school varchar(255) not null
+    id      bigserial primary key,
+    name    varchar(50)  not null,
+    surname varchar(50)  not null,
+    email   varchar(50)  not null,
+    course  varchar(10)  not null,
+    school  varchar(255) not null
 );
 
 create table public.roles
 (
-    id bigserial primary key,
+    id    bigserial primary key,
     title varchar(20)
 );
 
-create table public.users_roles(
-    id bigserial primary key ,
-    user_id bigint not null references public.users(id),
-    role_id bigint not null references public.roles(id),
+create table public.users_roles
+(
+    id      bigserial primary key,
+    user_id bigint not null references public.users (id),
+    role_id bigint not null references public.roles (id),
     unique (user_id, role_id)
 );
 
-insert into public.roles(title) values ('USER'), ('ADMIN'), ('SUPER_ADMIN');
+insert into public.roles(title)
+values ('USER'),
+       ('ADMIN'),
+       ('SUPER_ADMIN');
