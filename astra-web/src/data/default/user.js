ded66040f97ddbaad8088e3ae8f9215bd2edce6d
AndriiBosyk <andriybosik2002@ukr.net>
2022-08-03 19:10:11 +0300

    Fix local user role

--- astra-web/src/data/default/user.js
+++ astra-web/src/data/default/user.js
@@ -1,7 +1,7 @@
 import {userRole} from "../../constant/user.role";
 
 export const defaultUser = {
-    roles: [userRole.user],
+    roles: [userRole.admin],
     name: "Anonymous",
     surname: "Anonymous",
     email: "empty@email.com",
