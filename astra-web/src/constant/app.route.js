3d1086a6d6949192825841a0fb207703b800c141
Andrii Bosyk <55958195+AndriyBosik@users.noreply.github.com>
2022-08-03 22:24:54 +0300

    Implement user updating on frontend (#67)

--- astra-web/src/constant/app.route.js
+++ astra-web/src/constant/app.route.js
@@ -1,11 +1,14 @@
 import {env} from "./env";
 
+const baseUrl = env.baseApiUrl;
+
 export const route = {
-    signUp: `${env.baseApiUrl}/api/v1/auth/signup`,
-    token: `${env.baseApiUrl}/api/v1/auth/token`,
-    specializations: `${env.baseApiUrl}/api/v1/specializations`,
-    subjects: `${env.baseApiUrl}/api/v1/subjects`,
-    exams: `${env.baseApiUrl}/api/v1/exams`,
-    tests: `${env.baseApiUrl}/api/v1/tests`,
-    examinations: `${env.baseApiUrl}/api/v1/examinations`
+    signUp: `${baseUrl}/api/v1/auth/signup`,
+    token: `${baseUrl}/api/v1/auth/token`,
+    specializations: `${baseUrl}/api/v1/specializations`,
+    subjects: `${baseUrl}/api/v1/subjects`,
+    exams: `${baseUrl}/api/v1/exams`,
+    tests: `${baseUrl}/api/v1/tests`,
+    examinations: `${baseUrl}/api/v1/examinations`,
+    users: `${baseUrl}/api/v1/users`
 };
