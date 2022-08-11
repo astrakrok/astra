2fe11d209e36f183425068167eb8fef57d81a71c
AndriiBosyk <andriybosik2002@ukr.net>
2022-08-11 18:41:38 +0300

    Minor fix

--- astra-api/src/main/java/com/example/astraapi/meta/Endpoint.java
+++ astra-api/src/main/java/com/example/astraapi/meta/Endpoint.java
@@ -5,7 +5,6 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Endpoint {
   public static final String SPECIALIZATIONS = "/api/v1/specializations";
-  public static final String SUBJECTS = "/api/v1/subjects";
   public static final String EXAMS = "/api/v1/exams";
   public static final String TESTS = "/api/v1/tests";
   public static final String USERS = "/api/v1/users";
