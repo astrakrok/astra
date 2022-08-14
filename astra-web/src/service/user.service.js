08d52850b2656e76fe6fc1cc1344c1a3c028d843
Andrii Bosyk <55958195+AndriyBosik@users.noreply.github.com>
2022-08-14 23:45:44 +0300

    Add endpoint for updating user information (#89)

--- astra-web/src/service/user.service.js
+++ astra-web/src/service/user.service.js
@@ -1,7 +1,13 @@
 import {route} from "../constant/app.route";
+import {errorMessage} from "../error/message";
 import {client} from "../shared/js/axios";
 
 export const update = async user => {
-    const response = await client.put(route.users, user);
+    const response = await client.put(route.users, user)
+        .catch(() => ({
+            data: {
+                error: errorMessage.UNABLE_TO_UPDATE_USER_INFO
+            }
+        }));
     return response.data;
 }
