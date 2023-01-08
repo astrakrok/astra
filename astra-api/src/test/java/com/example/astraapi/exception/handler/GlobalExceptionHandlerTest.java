7bdc7dbf10ac415e668d8ee205631b6591717f6a
andriibosyk <andriybosik2002@ukr.net>
2023-01-08 14:46:37 +0200

    Tests fix

--- astra-api/src/test/java/com/example/astraapi/exception/handler/GlobalExceptionHandlerTest.java
+++ astra-api/src/test/java/com/example/astraapi/exception/handler/GlobalExceptionHandlerTest.java
@@ -2,7 +2,6 @@ package com.example.astraapi.exception.handler;
 
 import com.example.astraapi.dto.ErrorResponseDto;
 import com.example.astraapi.exception.AlreadyExistsException;
-import com.example.astraapi.meta.ErrorMessage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -17,18 +16,17 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(MockitoExtension.class)
 public class GlobalExceptionHandlerTest {
-  @InjectMocks
-  private GlobalExceptionHandler handler;
-  @Mock
-  private Logger logger;
+    @InjectMocks
+    private GlobalExceptionHandler handler;
+    @Mock
+    private Logger logger;
 
-  @Test
-  void shouldReturnResponseEntity() {
-    AlreadyExistsException exception = new AlreadyExistsException("Entity already exists");
-    ResponseEntity<ErrorResponseDto> responseEntity = handler.handleException(exception);
+    @Test
+    void shouldReturnResponseEntity() {
+        AlreadyExistsException exception = new AlreadyExistsException("Entity already exists");
+        ResponseEntity<ErrorResponseDto> responseEntity = handler.handleException(exception);
 
-    assertNotNull(responseEntity.getBody());
-    assertEquals(ErrorMessage.SOMETHING_WENT_WRONG.getMessage(), responseEntity.getBody().getError().getMessage());
-    assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, responseEntity.getStatusCode());
-  }
+        assertNotNull(responseEntity.getBody());
+        assertEquals(HttpStatus.CONFLICT, responseEntity.getStatusCode());
+    }
 }
